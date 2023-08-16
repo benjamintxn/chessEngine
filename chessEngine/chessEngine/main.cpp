e7490b9c8b7c41c98591259ef32200519c9e0784
benjamintxn <benetc2003@gmail.com>
2023-08-16 10:30:52 +0300

    Initial commit

---
+++ chessEngine/chessEngine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  chessEngine
+//
+//  Created by Ben Tan on 16/08/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
