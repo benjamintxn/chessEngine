b65b9311109451702fc74c7e26a46351ca7d6098
benjamintxn <benetc2003@gmail.com>
2023-08-16 10:52:49 +0300

    Testing commit

--- chessEngine/chessEngine/main.cpp
+++ chessEngine/chessEngine/main.cpp
@@ -1,14 +1,10 @@
-//
-//  main.cpp
-//  chessEngine
-//
-//  Created by Ben Tan on 16/08/2023.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+
+    cout << "Hello, World!\n";
+    
     return 0;
+    
 }
